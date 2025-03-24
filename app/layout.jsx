f118481643e526609b9a4aed3436cb458b5d7ec4
farzad kh <farzadkh900@gmail.com>
2025-03-25 03:04:10 +0330

    add(layout):viewport added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,14 @@
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
- 
+
 import Layout from "./components/layout/Layout";
- 
+import Head from "next/head";
 
 const inter = Work_Sans({
   subsets: ["latin"],
   weight: ["400", "600", "800"],
 });
 
-export const metadata = {
-  title: "Find Your Dream Home with Home Vision",
-  description: "Discover the best properties for sale and rent with Home Vision. Your dream home is just a search away.",
-  keywords: "home vision, real estate, buy house, rent apartment, property listings, dream home, real estate agency",
-  openGraph: {
-    title: "Find Your Dream Home with Home Vision",
-    description: "Discover the best properties for sale and rent with Home Vision. Your dream home is just a search away.",
-    type: "website",
-    url: "https://homevisions.vercel.app",
-    
-  },
-  viewport: "width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no",
-  charset: "UTF-8",
-};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -33,3 +19,26 @@ export default function RootLayout({ children }) {
   );
 }
 
+export const metadata = {
+  title: "Find Your Dream Home with Home Vision",
+  description:
+    "Discover the best properties for sale and rent with Home Vision. Your dream home is just a search away.",
+  keywords:
+    "home vision, real estate, buy house, rent apartment, property listings, dream home, real estate agency",
+  openGraph: {
+    title: "Find Your Dream Home with Home Vision",
+    description:
+      "Discover the best properties for sale and rent with Home Vision. Your dream home is just a search away.",
+    type: "website",
+    url: "https://homevisions.vercel.app",
+  },
+};
+
+export const viewport = {
+  width: "device-width",
+  height: "device-height",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: "no",
+};
--- app/page.jsx
+++ app/page.jsx
@@ -9,17 +9,17 @@ export default async function Home({}) {
 }
 export const revalidate = 60;
 
-export const metadata = {
-  title: "Home - Home Vision",
-  description:
-    "Discover the best real estate listings on Home Vision. Find your dream home and connect with property owners effortlessly.",
-  keywords:
-    "buy home, sell home, rent apartment, real estate, property listings, housing market",
-  openGraph: {
-    title: "Home - Home Vision",
-    description:
-      "Discover the best real estate listings on Home Vision. Find your dream home and connect with property owners effortlessly.",
-    type: "website",
-    url: "https://homevisions.vercel.app",
-  },
-};
+// export const metadata = {
+//   title: "Find Your Dream Home with Home Vision",
+//   description: "Discover the best properties for sale and rent with Home Vision. Your dream home is just a search away.",
+//   keywords: "home vision, real estate, buy house, rent apartment, property listings, dream home, real estate agency",
+//   openGraph: {
+//     title: "Find Your Dream Home with Home Vision",
+//     description: "Discover the best properties for sale and rent with Home Vision. Your dream home is just a search away.",
+//     type: "website",
+//     url: "https://homevisions.vercel.app",
+    
+//   },
+//   viewport: "width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no",
+//   charset: "UTF-8",
+// };
